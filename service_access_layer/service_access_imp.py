2babb1beff5920beae5f1739b6bff7fe51eae54a
mcballard <mattb315@yahoo.com>
2022-03-28 18:12:28 -0400

    add method to convert json from api to snake case and weed out incorrect datatypes for some inputs

--- service_access_layer/service_access_imp.py
+++ service_access_layer/service_access_imp.py
@@ -1,3 +1,6 @@
+import re
+from re import sub
+
 from Data_access_layer import dao_interface
 
 
@@ -11,10 +14,44 @@ class ServiceAccessIMP(ServiceAccessInterface):
     def __init__(self, dao_object: dao_interface):
         self.dao_obj = dao_object
 
+    def sanitize_json_from_api(self, json_from_api: dict) -> dict:
+        # this will change keys to snake_case and ensure they match database column names
+        # this will also ensure number strings are convertible to the appropriate datatypes (i.e. ids are not words)
+        # utilize regular expressions to parse the keys
+        snake_case_dictionary = {}
+        for key in json_from_api:
+            snake_key = '_'.join(
+                sub('([A-Z][a-z]+)', r' \1', sub('([A-Z]+)', r' \1', key.replace('-', ' '))).split()).lower()
+            snake_case_dictionary[snake_key] = json_from_api[key]
+        # application specific search for keys with _id should be implemented here including for checks
+        # on any other number type expected key, value pairs to ensure values are convertible types
+        for key in snake_case_dictionary:
+            if re.search("_id", key):
+                try:
+                    proper_int_format = int(snake_case_dictionary[key])
+                    snake_case_dictionary[key] = proper_int_format
+                except ValueError as e:
+                    raise FailedTransaction("The input from the api is not convertible to integer for an id field.")
+        if type(snake_case_dictionary["table_name"]) is not str:
+            raise FailedTransaction("The field containing the table name is in the wrong format, must be a string.")
+        # the following regular expression should be used on any string value entering the db to prevent sql injection
+        snake_case_dictionary["table_name"] = re.sub("[^A-Za-z_0-9]", "", snake_case_dictionary["table_name"])
+        try:
+            check_for_int_as_table_name = int(snake_case_dictionary["table_name"])
+            if type(check_for_int_as_table_name) is int:
+                raise FailedTransaction("The table name should not be a number")
+            check_for_float_as_table_name = float(snake_case_dictionary["table_name"])
+            if type(check_for_float_as_table_name) is float:
+                raise FailedTransaction("The table name should not be a number")
+        except ValueError as e:
+            # confirms cannot convert and is not a number normal execution should continue
+            if e is ValueError:
+                return snake_case_dictionary
+        return snake_case_dictionary
+
     def service_create_reimbursement_request(self, entity_dictionary: dict) -> RowEntity:
         if len(entity_dictionary["request_comment"]) > 100:
             raise FailedTransaction("test service reimbursement request should not exceed 100")
-
 
     def service_cancel_reimbursement_request(self, entity_dictionary: dict) -> bool:
         if type(entity_dictionary["reimbursement_request_id"]) == int:
--- tests/service_tests/test_service_access_imp.py
+++ tests/service_tests/test_service_access_imp.py
@@ -1,10 +1,55 @@
-
 from Data_access_layer.dao_imp import DAOImp
 from custom_exceptions.failed_transaction import FailedTransaction
+from data_entity_class.row_entity import RowEntity
 from service_access_layer.service_access_imp import ServiceAccessIMP
 
 test_dao = DAOImp()
 test_service = ServiceAccessIMP(test_dao)
+
+
+def test_sanitize_json_from_api_success():
+    test_dict = {
+        "tableName": "tablename",
+        "tableNameId": 1
+    }
+    assert test_service.sanitize_json_from_api(test_dict)["table_name_id"] == 1
+
+
+def test_sanitize_json_from_api_non_string_table_name():
+    try:
+        test_dict = {
+            "tableName": 5,
+            "tableNameId": 1
+        }
+        test_service.sanitize_json_from_api(test_dict)
+        assert False
+    except FailedTransaction as e:
+        assert str(e) == "The field containing the table name is in the wrong format, must be a string."
+
+
+def test_sanitize_json_from_api_convertible_number():
+    try:
+        test_dict = {
+            "tableName": "5",
+            "tableNameId": 1
+        }
+        test_service.sanitize_json_from_api(test_dict)
+        assert False
+    except FailedTransaction as e:
+        assert str(e) == "The table name should not be a number"
+
+
+def test_sanitize_json_from_api_id_field_non_number():
+    try:
+        test_dict = {
+            "tableName": "tablename",
+            "tableNameId": "one"
+        }
+        test_service.sanitize_json_from_api(test_dict)
+        assert False
+    except FailedTransaction as e:
+        assert str(e) == "The input from the api is not convertible to integer for an id field."
+
 
 def test_service_create_reimbursement_request_comment_less_than_100():
     try:
@@ -41,7 +86,7 @@ def test_service_cancel_reimbursement_request_success():
 def test_service_select_total_amount_requested_employee_id_non_numeric():
     try:
         service_access_input = ServiceAccessIMP(test_dao)
-        service_access_input_test_employee_id_non_numeric = {"request_employee_id": '345676765432'}
+        service_access_input_test_employee_id_non_numeric = {"employee_id": '345676765432'}
         test_entity = RowEntity(service_access_input_test_employee_id_non_numeric)
         tests.service_tests.service_access_input_test_employee_id_non_numeric
         assert False

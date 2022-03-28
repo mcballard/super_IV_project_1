2941e1b0ba17c80382fe2af26ca22c8916b92300
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2022-03-28 16:43:35 -0500

    cleaned up Samantha's test

--- tests/service_tests/test_service_access_imp.py
+++ tests/service_tests/test_service_access_imp.py
@@ -40,10 +40,8 @@ def test_service_cancel_reimbursement_request_success():
 
 def test_service_select_total_amount_requested_employee_id_non_numeric():
     try:
-        service_access_input = ServiceAccessIMP(test_dao)
-        service_access_input_test_employee_id_non_numeric = {"request_employee_id": '345676765432'}
-        test_entity = RowEntity(service_access_input_test_employee_id_non_numeric)
-        tests.service_tests.service_access_input_test_employee_id_non_numeric
+        bad_input = {"employee_id": '345676765432'}
+        test_service.service_select_total_amount_requested(bad_input)
         assert False
     except FailedTransaction as e:
         assert str(e) == "test reimbursement employee_id cannot use numeric type"

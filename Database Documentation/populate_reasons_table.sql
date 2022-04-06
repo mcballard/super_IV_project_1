9b5b2718e005d73fd4065084c8f0eb61d1542a53
mcballard <mattb315@yahoo.com>
2022-04-06 18:02:39 -0400

    made changes for db reasons, added postman export for postman tests

--- Database Documentation/populate_reasons_table.sql
+++ Database Documentation/populate_reasons_table.sql
@@ -1,5 +1,5 @@
-insert into project_one_sandbox.reasons_table values (default,'Travel');
-insert into project_one_sandbox.reasons_table values (default,'Ammunition');
-insert into project_one_sandbox.reasons_table values (default,'Food');
-insert into project_one_sandbox.reasons_table values (default,'Vetrenarian');
-insert into project_one_sandbox.reasons_table values (default,'Equipment');
+insert into project_one_sandbox.reasons_table values (default,'Elaborate Props');
+insert into project_one_sandbox.reasons_table values (default,'Elaborate Disguises');
+insert into project_one_sandbox.reasons_table values (default,'Elaborate Scheme Expenses');
+insert into project_one_sandbox.reasons_table values (default,'New White Outfit');
+insert into project_one_sandbox.reasons_table values (default,'New Black Outfit');

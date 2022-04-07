9417a96b0829a28dc131d3397807636914aa9fcd
mcballard <mattb315@yahoo.com>
2022-04-07 11:12:49 -0400

    moved logs to central location in documentation folder

--- Database Documentation/populate_reasons_table.sql
+++ Database Documentation/populate_reasons_table.sql
@@ -1,5 +1,5 @@
 insert into project_one_sandbox.reasons_table values (default,'Elaborate Props');
 insert into project_one_sandbox.reasons_table values (default,'Elaborate Disguises');
-insert into project_one_sandbox.reasons_table values (default,'Elaborate Scheme Expenses');
+insert into project_one_sandbox.reasons_table values (default,'Elaborate Traps');
 insert into project_one_sandbox.reasons_table values (default,'New White Outfit');
 insert into project_one_sandbox.reasons_table values (default,'New Black Outfit');
